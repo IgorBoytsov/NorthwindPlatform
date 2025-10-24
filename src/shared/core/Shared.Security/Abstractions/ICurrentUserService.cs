062fb8295d6c3958de631dcaaa1e25d886ed665d
Light Play <lightplay456@gmail.com>
2025-10-24 13:21:49 +0300

    feat(security): определить контракты безопасности, включая интерфейсы служб и DTO

---
+++ src/shared/core/Shared.Security/Abstractions/ICurrentUserService.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Claims;
+
+namespace Shared.Security.Abstractions
+{
+    public interface ICurrentUserService
+    {
+        Guid? UserId { get; }
+        string? UserEmail { get; }
+        bool IsAuthenticated { get; }
+        bool IsInRole(string roleName);
+        IEnumerable<Claim> GetUserClaims();
+    }
+}

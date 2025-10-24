c9b11b38171ca1fe5474e9355636276c36172c84
Light Play <lightplay456@gmail.com>
2025-10-24 13:46:11 +0300

    feat(security): добавлен контракт для контекста текущего пользователя

---
+++ src/clients/shared/core/Shared.Client.Security/Abstractions/IUserContext.cs
@@ -0,0 +1,24 @@
+﻿using Shared.Contracts.Responses.Security;
+using System.Security.Claims;
+
+namespace Shared.Client.Security.Abstractions
+{
+    public interface IUserContext
+    {
+        bool IsAuthenticated { get; }
+        Guid UserId { get; }
+        string? Email { get; }
+        string? UserName { get; }
+        string? FirstName { get; }
+        string? LastName { get; }
+
+        IReadOnlyCollection<string> Roles { get; }
+        IReadOnlyCollection<Claim> Claims { get; }
+
+        bool IsInRole(string roleName);
+        string? GetClaimValue(string claimType);
+
+        void SetUser(AuthenticationResponse authResponse);
+        void ClearUser();
+    }
+}

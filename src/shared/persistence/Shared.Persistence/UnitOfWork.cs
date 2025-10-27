6a585ce13d9f20622d988a84a15bb0565fa01b9c
Light Play <lightplay456@gmail.com>
2025-10-27 14:25:11 +0300

    feat(shared.persistence): добавить базовые компоненты

---
+++ src/shared/persistence/Shared.Persistence/UnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.Application.Data;
+
+namespace Shared.Persistence
+{
+    public class UnitOfWork<TContext>(TContext context) : IUnitOfWork
+        where TContext : DbContext, IBaseWriteDbContext
+    {
+        private readonly TContext _context = context;
+
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+            => _context.SaveChangesAsync(cancellationToken);
+    }
+}

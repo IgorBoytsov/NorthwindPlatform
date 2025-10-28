1dfaee74f96689030dbf91d94bba106c27f31e3c
Light Play <lightplay456@gmail.com>
2025-10-28 14:27:38 +0300

    feat(shared.application): добавить абстракции для данных и исключений в Shared.Application

---
+++ src/shared/core/Shared.Application/Data/IBaseWriteDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Shared.Application.Data
+{
+    public interface IBaseWriteDbContext
+    {
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
+    }
+}
---
+++ src/shared/core/Shared.Application/Data/IUnitOfWork.cs
@@ -0,0 +1,7 @@
+﻿namespace Shared.Application.Data
+{
+    public interface IUnitOfWork
+    {
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}

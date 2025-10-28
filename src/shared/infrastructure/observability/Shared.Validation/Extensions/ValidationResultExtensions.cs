80ebd5ec8d33ef9143b62856aec2b22ec1693a85
Light Play <lightplay456@gmail.com>
2025-10-28 14:35:38 +0300

    feat(shared.validation): добавить общую библиотеку валидации

---
+++ src/shared/infrastructure/observability/Shared.Validation/Extensions/ValidationResultExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Common.Core.Results;
+using FluentValidation.Results;
+
+namespace Shared.Validation.Extensions
+{
+    public static class ValidationResultExtensions
+    {
+        public static bool TryGetFailureResult<TResult>(this ValidationResult validationResult, out Result<TResult> failureResult)
+        {
+            if (validationResult.IsValid)
+            {
+                failureResult = null!;
+                return false;
+            }
+
+            var validationErrors = validationResult.Errors
+                .Select(e => new Error(ErrorCode.Validation, $"Ошибка валидации: {e.ErrorMessage}"));
+
+            failureResult = Result<TResult>.Failure(validationErrors);
+            return true;
+        }
+
+        public static bool TryGetFailureResult(this ValidationResult validationResult, out Result failureResult)
+        {
+            if (validationResult.IsValid)
+            {
+                failureResult = null!;
+                return false;
+            }
+
+            var validationErrors = validationResult.Errors
+                .Select(e => new Error(ErrorCode.Validation, $"Ошибка валидации: {e.ErrorMessage}"))
+                .ToList();
+
+            failureResult = Result.Failure(validationErrors);
+            return true;
+        }
+    }
+}
---
+++ src/shared/infrastructure/observability/Shared.Validation/Rules/IdValidation/GuidValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace Shared.Validation.Rules.IdValidation
+{
+    public sealed class GuidValidator<T> : AbstractValidator<T> where T : IHasGuidId
+    {
+        public GuidValidator()
+            => RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("Идентификатор (GUID) не может быть пустым");
+    }
+}

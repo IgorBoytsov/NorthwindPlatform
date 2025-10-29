5f7eac7f9ffa7b049713b9aba05ff77f21f6a9b2
Light Play <lightplay456@gmail.com>
2025-10-29 16:53:36 +0300

    feat: Создана начальная структура проекта для разработки аутентификации в Android приложение

---
+++ android/app/src/main/java/com/northwind/platform/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.northwind.platform
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import com.northwind.platform.ui.theme.NorthwindPlatformTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NorthwindPlatformTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Hello!!! $name!",
+            color = Color.White,
+            fontFamily = FontFamily.Cursive,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxSize()
+        )
+    }
+}
+
+@Preview(
+    showBackground = true,
+    backgroundColor = 0x00262424
+)
+@Composable
+fun GreetingPreview() {
+    NorthwindPlatformTheme {
+        Greeting("Мир!")
+    }
+}

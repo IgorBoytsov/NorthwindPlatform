cd8bba96c0f68a7f61aaf53beab96326f273389c
Light Play <lightplay456@gmail.com>
2026-02-15 16:43:13 +0300

    refactor(staff-portal): Перенесена папка features из app в src

--- web/apps/staff-portal/src/app/app.routes.ts
+++ web/apps/staff-portal/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Route } from '@angular/router';
-import { LoginComponent } from './features/auth/login/login.component';
+import { LoginComponent } from '../features/auth/login/login.component';
 
 export const appRoutes: Route[] = [
     { path: 'login', component: LoginComponent },
